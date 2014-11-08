530f02363884a6b36ec993c699c79d85c27d2908
Khoi Tran <emails.khoi@gmail.com>
2014-11-08 16:59:24 -0500

    create chat server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res){
   res.send("This is the chat server");
