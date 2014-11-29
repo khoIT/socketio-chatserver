e93a218597987f0d397ebc1f85e02c6ff0b878a3
Khoi Tran <emails.khoi@gmail.com>
2014-11-28 20:45:53 -0500

    finish logic checkroom

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ var rooms = ['mainRoom'];
 
 io.sockets.on('connection', function (socket) {
 
-  console.log("User connected");
   // when the client emits 'adduser', this listens and executes
   socket.on('addUser', function(username){
     // store the username in the socket session for this client
@@ -34,6 +33,7 @@ io.sockets.on('connection', function (socket) {
     socket.join('mainRoom');
     // echo to client they've connected
     socket.emit('updatechat', 'SERVER', 'you have connected to mainRoom');
+
     // echo to room 1 that a person has connected to their room
     socket.broadcast.to('mainRoom').emit('updatechat', 'SERVER', username + ' has connected to this room');
     socket.emit('updaterooms', rooms, 'mainRoom');
@@ -46,7 +46,6 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('newRoom', function(newroom){
-    console.log("User change room");
     // leave the current room (stored in session)
     socket.leave(socket.room);
     // join new room, received as function parameter
@@ -58,6 +57,10 @@ io.sockets.on('connection', function (socket) {
     socket.room = newroom;
     socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
     socket.emit('updaterooms', rooms, newroom);
+  });
+  socket.on('checkRoom', function(){
+    socket.emit('checkRoom', socket.room);
+    console.log('Current room: ' + socket.room);
   });
 
   // when the user disconnects.. perform this
